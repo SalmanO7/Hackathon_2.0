595fd53436cb23330bcc0314c89bca72a1af8c7d
SalmanO7 <msalman221020@gmail.com>
2025-02-05 04:46:51 +0500

    complete stripe setup

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,11 +1,9 @@
 import dynamic from 'next/dynamic'
-import React from 'react'
 
 const DynamicComponentWithNoSSR = dynamic(
     () => import('../components/payment/StripePayment'),
     { ssr: true }
 )
-
 
 const CheckoutRoute = () => {
     return (
