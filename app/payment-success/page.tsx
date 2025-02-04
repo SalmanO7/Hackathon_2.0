0566864501b7072442bb57ae357c271dd82e7674
SalmanO7 <msalman221020@gmail.com>
2025-02-05 04:22:02 +0500

    complete stripe setup

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,15 +1,16 @@
 import Navbar from "../pages/Navbar"
 
 interface IParams {
-    searchParams: {
-        amount: number
-    }
+    searchParams: Record<string, string | undefined>;
 }
 const PaymentSuccess = ({ searchParams }: IParams) => {
+
+    const amount = parseFloat(searchParams.amount || "80");
+
     return (
         <div className="text-center w-full">
             <Navbar />
-            <h1 className="text-6xl mt-16">Thank you for purchasing $ {searchParams.amount}</h1>
+            <h1 className="text-6xl mt-16">Thank you for purchasing $ {amount}</h1>
         </div>
     )
 }
